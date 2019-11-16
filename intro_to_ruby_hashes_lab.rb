03129fbe41d88960b9d9e9c9051f39b8c23265c5
Cindy Kei <myk245@nyu.edu>
2019-11-16 22:29:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,11 +28,11 @@ end
 
 def read_from_hash(hash, key)
   return hash[key]
-end
+
 
 def update_counting_hash(hash, key)
   if hash[key]
     hash[key] += 1
   else
     hash[key] = 1
-  end 
+  end
